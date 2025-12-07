3c51baaab29b7a9b1ef7f440dc78f487bca4e320
adnantaro <t4rotar0@gmail.com>
2025-12-07 14:17:12 +0700

    fix import paths src

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import eventRoutes from './routes/eventRoutes.js';
+import eventRoutes from './src/routes/eventRoutes.js';
 
 dotenv.config();
 
